4d1042dff7aab803ecd52b408f6023668a1019b4
MaryaRitaSanches <marya.souza4@etec.sp.gov.br>
2025-06-10 20:01:43 -0300

    alterações

---
+++ AgendaBarbearia/FormularioADM.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AgendaBarbearia
+{
+    partial class FormularioADM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            linkLabel1 = new LinkLabel();
+            botaoEntrada = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(282, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(210, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Faça o seu login de ADM";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 111);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 31);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(282, 180);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(214, 31);
+            textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(222, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(216, 183);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Senha";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(178, 258);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(405, 25);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Caso você não possua cadastro. Se cadastre aqui.";
+            // 
+            // botaoEntrada
+            // 
+            botaoEntrada.Location = new Point(325, 221);
+            botaoEntrada.Name = "botaoEntrada";
+            botaoEntrada.Size = new Size(112, 34);
+            botaoEntrada.TabIndex = 6;
+            botaoEntrada.Text = "Entrar";
+            botaoEntrada.UseVisualStyleBackColor = true;
+            botaoEntrada.Click += botaoEntrada_Click;
+            // 
+            // FormularioADM
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PowderBlue;
+            ClientSize = new Size(800, 450);
+            Controls.Add(botaoEntrada);
+            Controls.Add(linkLabel1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "FormularioADM";
+            Text = "FormularioADM";
+            Load += FormularioADM_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private LinkLabel linkLabel1;
+        private Button botaoEntrada;
+    }
+}
--- AgendaBarbearia/FormularioADM.cs
+++ AgendaBarbearia/FormularioADM.cs
@@ -1,20 +1,21 @@
+ï»¿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
 namespace AgendaBarbearia
 {
-    public partial class Form1 : Form
+    public partial class FormularioADM : Form
     {
-        public Form1()
+        public FormularioADM()
         {
             InitializeComponent();
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -22,18 +23,9 @@ namespace AgendaBarbearia
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void FormularioADM_Load(object sender, EventArgs e)
         {
 
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
         }
 
         private void botaoEntrada_Click(object sender, EventArgs e)
@@ -41,8 +33,8 @@ namespace AgendaBarbearia
             string emailTexto = email.Text;
             string senhaTexto = senha.Text;
 
-            // Exemplo de validação simples (você pode trocar por banco depois)
-            if (email.Text == "adm@exemplo.com" && senha.Text == "345")
+            // Exemplo de validaÃ§Ã£o simples (vocÃª pode trocar por banco depois)
+            if (email.Text == "cliente@exemplo.com" && senha.Text == "123")
             {
                 FormAgendamento formAgendamento = new FormAgendamento();
                 formAgendamento.Show();  // Abre a tela de agendamento
@@ -54,10 +46,9 @@ namespace AgendaBarbearia
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            // botão do adm
-            
+
         }
     }
 }
